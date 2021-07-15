62011a2f1b16feb9af24fd69c097675701f32462
oe.kazuma <oe.kazuma@a-tm.co.jp>
2021-07-15 18:46:11 +0900

    revert config

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,7 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    package: {
-      exports: {
-        include: ['index.ts']
-      }
-    }
-  }
+  preprocess: preprocess()
 };
 
 export default config;
