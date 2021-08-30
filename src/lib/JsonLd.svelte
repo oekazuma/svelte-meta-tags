43698ab9b98781f3b9ff246640c1e29ab772ba75
oekazuma <koneko0604@gmail.com>
2021-08-31 00:28:57 +0900

    feat: separate JSON-LD from MetaTags component

---
+++ src/lib/JsonLd.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let schema = undefined;
+</script>
+
+<svelte:head>
+  {#if schema}
+    {@html `<script type="application/ld+json">${
+      JSON.stringify({ '@context': 'https://schema.org', ...schema }) + '<'
+    }/script>`}
+  {/if}
+</svelte:head>
