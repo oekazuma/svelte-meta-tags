5eeaf68c1c0454c3619fe3f30e7b2c5521431334
Jack Weilage <jack@weilage.dev>
2022-05-26 11:50:00 -0700

    Use reactive statements
    The current solution (using an arrow function) is unnecessary. A reactive string works the same, but also updates if the JSON needs to be updated after the component is mounted.

--- src/lib/JsonLd.svelte
+++ src/lib/JsonLd.svelte
@@ -2,15 +2,15 @@
   export let output = 'head';
   export let schema = undefined;
 
-  const jsonSchema = (schema) => JSON.stringify({ '@context': 'https://schema.org', ...schema });
+  $: jsonSchema = JSON.stringify({ '@context': 'https://schema.org', ...schema });
 </script>
 
 <svelte:head>
   {#if schema && output === 'head'}
-    {@html `${'<scri' + 'pt type="application/ld+json">'}${jsonSchema(schema)}${'</scri' + 'pt>'}`}
+    {@html `${'<scri' + 'pt type="application/ld+json">'}${jsonSchema}${'</scri' + 'pt>'}`}
   {/if}
 </svelte:head>
 
 {#if schema && output === 'body'}
-  {@html `${'<scri' + 'pt type="application/ld+json">'}${jsonSchema(schema)}${'</scri' + 'pt>'}`}
+  {@html `${'<scri' + 'pt type="application/ld+json">'}${jsonSchema}${'</scri' + 'pt>'}`}
 {/if}
