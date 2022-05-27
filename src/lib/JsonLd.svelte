bc7b4bce8cc29a5b770b01170b3ba64687bda001
Kazuma Oe <koneko0604@gmail.com>
2022-05-27 00:59:01 +0000

    chore: code format

--- src/lib/JsonLd.svelte
+++ src/lib/JsonLd.svelte
@@ -3,7 +3,10 @@
   export let schema = {};
 
   $: isValid = schema && typeof schema === 'object';
-  $: json = `${'<scri' + 'pt type="application/ld+json">'}${JSON.stringify({ '@context': 'https://schema.org', ...schema })}${'</scri' + 'pt>'}`;
+  $: json = `${'<scri' + 'pt type="application/ld+json">'}${JSON.stringify({
+    '@context': 'https://schema.org',
+    ...schema
+  })}${'</scri' + 'pt>'}`;
 </script>
 
 <svelte:head>
