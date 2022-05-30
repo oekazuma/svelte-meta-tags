6e1144a2cc0a6219d1d1370a3ddbae15348e93af
oe.kazuma <koneko0604@gmail.com>
2022-05-30 14:19:26 +0900

    chore: change to use `config.kit.alias`

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
     adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          'svelte-meta-tags': path.resolve('src/lib')
-        }
-      }
+    alias: {
+      'svelte-meta-tags': 'src/lib'
     }
   }
 };
