30713f565d5f1763d78cf2c67aa1fb608114a4ea
oe.kazuma <koneko0604@gmail.com>
2022-06-02 14:19:33 +0900

    ci: clean up unnecessary settings

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,12 +4,8 @@ export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      /* eslint-disable @typescript-eslint/no-var-requires */
-      return require('./cypress/plugins/index.cjs')(on, config);
-    },
+    setupNodeEvents(on, config) {}, // eslint-disable-line
+    supportFile: false,
     baseUrl: 'http://localhost:3000',
   },
 });
