6c35be432f192096cd485e5aeb190ffffdf4f302
oe.kazuma <koneko0604@gmail.com>
2022-12-12 09:29:01 +0900

    chore: change svelte-preprocess to vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     alias: {
