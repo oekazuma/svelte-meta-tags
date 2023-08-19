98add2bd0ddb9314e0ed8cec68600980f2207fe0
oe.kazuma <koneko0604@gmail.com>
2023-08-19 21:19:49 +0900

    chore: change repository to monorepo to be able to test each of Svelte v3 and v4

--- tests/v3/tests/robotsAnother.test.ts
+++ tests/v3/tests/robotsAnother.test.ts
@@ -1,15 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('Another Robots props SEO applied correctly', async ({ page }) => {
-  const consoleMessages = [];
+const checkConsoleMessagesFor = (expectedMessage) => (msg) => {
+  return msg.text().includes(expectedMessage);
+};
 
-  page.on('console', (msg) => {
-    consoleMessages.push(msg.text());
+test('Another Robots props SEO applied correctly', async ({ page }) => {
+  let isMessageFound = false;
+
+  page.on('console', msg => {
+    isMessageFound = checkConsoleMessagesFor('additionalRobotsProps cannot be used when robots is set to false')(msg);
   });
 
   await page.goto('/robots/another');
   await expect(page).toHaveTitle('Another Robots meta title | Svelte Meta Tags');
   await expect(page.locator('h1')).toContainText('Another Robots props SEO');
   await expect(page.locator('head meta[name="robots"]')).toHaveCount(0);
-  expect(consoleMessages).toContain('additionalRobotsProps cannot be used when robots is set to false');
+
+  expect(isMessageFound).toBeTruthy();
 });
