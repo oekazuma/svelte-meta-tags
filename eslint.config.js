802c854146a2cb7951c04958374b094058790e02
oekazuma <koneko0604@gmail.com>
2024-04-12 10:38:22 +0900

    chore: update eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser
+      }
+    },
+    rules: {
+      'svelte/no-at-html-tags': 'off'
+    }
+  },
+  {
+    ignores: ['**/build/', '**/.svelte-kit/', '**/dist/']
+  }
+];
