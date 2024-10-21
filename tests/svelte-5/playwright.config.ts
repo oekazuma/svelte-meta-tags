a72584efed9cab26590d4ad0e33ce10bf764b8d0
oekazuma <koneko0604@gmail.com>
2024-10-21 10:13:35 +0900

    chore: revert playwright config

--- tests/svelte-5/playwright.config.ts
+++ tests/svelte-5/playwright.config.ts
@@ -2,7 +2,7 @@ import { devices, type PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   timeout: process.env.CI ? 45_000 : 15_000,
-  retries: 5,
+  retries: process.env.CI ? 5 : 0,
   workers: process.env.CI ? 2 : undefined,
   webServer: {
     command: 'vite build && vite preview --port 4000',
