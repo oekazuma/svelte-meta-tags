5e6fba6c24c86ad3701c72e975a50eae89609f93
oekazuma <koneko0604@gmail.com>
2024-10-21 13:09:29 +0900

    refactor: remove radash

--- packages/svelte-meta-tags/src/lib/deepMerge.ts
+++ packages/svelte-meta-tags/src/lib/deepMerge.ts
@@ -1,6 +1,38 @@
-import { assign } from 'radash';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function deepMerge(target: any, source: any) {
+  const sourceKeys = Object.keys(source);
 
-type MergeParams = Parameters<typeof assign>;
-export const deepMerge = (object1: MergeParams[1], object2: MergeParams[1]): ReturnType<typeof assign> => {
-  return assign(object1, object2);
-};
+  for (let i = 0; i < sourceKeys.length; i++) {
+    const key = sourceKeys[i];
+
+    const sourceValue = source[key];
+    const targetValue = target[key];
+
+    if (Array.isArray(sourceValue)) {
+      if (Array.isArray(targetValue)) {
+        target[key] = deepMerge(targetValue, sourceValue);
+      } else {
+        target[key] = deepMerge([], sourceValue);
+      }
+    } else if (isPlainObject(sourceValue)) {
+      if (isPlainObject(targetValue)) {
+        target[key] = deepMerge(targetValue, sourceValue);
+      } else {
+        target[key] = deepMerge({}, sourceValue);
+      }
+    } else if (targetValue === undefined || sourceValue !== undefined) {
+      target[key] = sourceValue;
+    }
+  }
+
+  return target;
+}
+
+function isPlainObject(object?: unknown): boolean {
+  if (typeof object !== 'object' || object === null) {
+    return false;
+  }
+
+  const proto = Object.getPrototypeOf(object);
+  return proto === Object.prototype || proto === null;
+}
