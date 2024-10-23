6cb836713fb77f2eba3207b8b04fd367e0288070
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-23 10:52:27 +0100

    feat(twitter): add test

---
+++ tests/svelte-5/src/routes/twitter/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { MetaTags } from 'svelte-meta-tags';
+</script>
+
+<MetaTags
+  twitter={{
+    cardType: 'summary_large_image',
+    site: '@site',
+    description: 'twitter test',
+    creator: '@handle',
+    creatorId: '1234567890',
+    image: 'https://www.test.ie/og-image-book-title-01.jpg',
+    imageAlt: 'book',
+    player: 'https://www.test.ie/sample-image.jpg',
+    playerHeight: 100,
+    playerWidth: 100,
+    playerStream: 'https://www.test.ie/sample.mp4',
+    appNameIphone: 'meta-tags',
+    appIdIphone: '12345',
+    appUrlIphone: 'https://www.test.ie/og-image-book-title-01.jpg',
+    appNameIpad: 'meta-tags',
+    appIdIpad: '12345',
+    appUrlIpad: 'https://www.test.ie/og-image-book-title-01.jpg',
+    appNameGoogleplay: 'meta-tags',
+    appIdGoogleplay: 'com.meta.tags',
+    appUrlGoogleplay: 'https://www.test.ie/og-image-book-title-01.jpg'
+  }}
+/>
+
+<h1>Twitter SEO</h1>
