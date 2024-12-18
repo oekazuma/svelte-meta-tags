d58564519bfe0a44e3f5385946f40d57a44aea09
oekazuma <koneko0604@gmail.com>
2024-12-18 15:11:13 +0900

    feat: update site and base configuration in astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://oekazuma.github.io/',
+  base: import.meta.env.DEV ? '/' : '/svelte-meta-tags',
   integrations: [
     starlight({
       title: 'SvelteMetaTags',
