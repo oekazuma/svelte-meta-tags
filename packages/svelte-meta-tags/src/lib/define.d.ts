e03f4a9ecc5f532aa2731f2e9d969d1c2d101c42
oekazuma <oe.kazuma@a-tm.co.jp>
2025-10-21 16:39:22 +0900

    feat: add defineBaseMetaTags and definePageMetaTags functions for meta tags management

---
+++ packages/svelte-meta-tags/src/lib/define.d.ts
@@ -0,0 +1,9 @@
+import type { MetaTagsProps } from './types.js';
+
+export declare const defineBaseMetaTags: (obj: MetaTagsProps) => {
+  readonly props: Readonly<MetaTagsProps>;
+};
+
+export declare const definePageMetaTags: (obj: MetaTagsProps) => {
+  readonly props: Readonly<MetaTagsProps>;
+};
