23abe801a455fb0c5640075e23a0e50af4614450
Scott Wu <sw@scottwu.ca>
2026-06-27 10:25:16 +0800

    refactor: simplify titleTemplate evaluation logic

--- packages/svelte-meta-tags/src/lib/MetaTags.svelte
+++ packages/svelte-meta-tags/src/lib/MetaTags.svelte
@@ -18,7 +18,7 @@
     additionalLinkTags = undefined
   }: Partial<MetaTagsProps> = $props();
 
-  let updatedTitle = $derived(titleTemplate ? (title ? titleTemplate.replace(/%s/g, title) : title) : title);
+  let updatedTitle = $derived(title && (titleTemplate?.replace(/%s/g, title) ?? title));
 
   let robotsParams = $derived.by(() => {
     if (!additionalRobotsProps) return '';
@@ -72,9 +72,9 @@
     <link rel="alternate" media={mobileAlternate.media} href={mobileAlternate.href} />
   {/if}
 
-    {#each languageAlternates as languageAlternate (languageAlternate)}
-      <link rel="alternate" hrefLang={languageAlternate.hrefLang} href={languageAlternate.href} />
-    {/each}
+  {#each languageAlternates as languageAlternate (languageAlternate)}
+    <link rel="alternate" hrefLang={languageAlternate.hrefLang} href={languageAlternate.href} />
+  {/each}
 
   {#if twitter}
     {#if twitter.cardType}
@@ -171,8 +171,8 @@
         {/if}
       {:else if openGraph.type.toLowerCase() === 'book' && openGraph.book}
         {#each openGraph.book?.authors as author (author)}
-            <meta property="book:author" content={author} />
-          {/each}
+          <meta property="book:author" content={author} />
+        {/each}
 
         {#if openGraph.book.isbn}
           <meta property="book:isbn" content={openGraph.book.isbn} />
@@ -183,8 +183,8 @@
         {/if}
 
         {#each openGraph.book?.tags as tag (tag)}
-            <meta property="book:tag" content={tag} />
-          {/each}
+          <meta property="book:tag" content={tag} />
+        {/each}
       {:else if openGraph.type.toLowerCase() === 'article' && openGraph.article}
         {#if openGraph.article.publishedTime}
           <meta property="article:published_time" content={openGraph.article.publishedTime} />
@@ -199,33 +199,33 @@
         {/if}
 
         {#each openGraph.article?.authors as author (author)}
-            <meta property="article:author" content={author} />
-          {/each}
+          <meta property="article:author" content={author} />
+        {/each}
 
         {#if openGraph.article.section}
           <meta property="article:section" content={openGraph.article.section} />
         {/if}
 
         {#each openGraph.article?.tags as tag (tag)}
-            <meta property="article:tag" content={tag} />
-          {/each}
+          <meta property="article:tag" content={tag} />
+        {/each}
       {:else if openGraph.type.toLowerCase() === 'video.movie' || openGraph.type.toLowerCase() === 'video.episode' || openGraph.type.toLowerCase() === 'video.tv_show' || (openGraph.type.toLowerCase() === 'video.other' && openGraph.video)}
         {#each openGraph.video?.actors as actor (actor)}
-            {#if actor.profile}
-              <meta property="video:actor" content={actor.profile} />
-            {/if}
-            {#if actor.role}
-              <meta property="video:actor:role" content={actor.role} />
-            {/if}
-          {/each}
+          {#if actor.profile}
+            <meta property="video:actor" content={actor.profile} />
+          {/if}
+          {#if actor.role}
+            <meta property="video:actor:role" content={actor.role} />
+          {/if}
+        {/each}
 
         {#each openGraph.video?.directors as director (director)}
-            <meta property="video:director" content={director} />
-          {/each}
+          <meta property="video:director" content={director} />
+        {/each}
 
         {#each openGraph.video?.writers as writer (writer)}
-            <meta property="video:writer" content={writer} />
-          {/each}
+          <meta property="video:writer" content={writer} />
+        {/each}
 
         {#if openGraph.video?.duration}
           <meta property="video:duration" content={openGraph.video.duration.toString()} />
@@ -236,8 +236,8 @@
         {/if}
 
         {#each openGraph.video?.tags as tag (tag)}
-            <meta property="video:tag" content={tag} />
-          {/each}
+          <meta property="video:tag" content={tag} />
+        {/each}
 
         {#if openGraph.video?.series}
           <meta property="video:series" content={openGraph.video.series} />
@@ -275,31 +275,31 @@
       {/each}
     {/if}
 
-      {#each openGraph.videos as video (video)}
-        <meta property="og:video" content={video.url} />
-        {#if video.width}
-          <meta property="og:video:width" content={video.width.toString()} />
-        {/if}
-        {#if video.height}
-          <meta property="og:video:height" content={video.height.toString()} />
-        {/if}
-        {#if video.secureUrl}
-          <meta property="og:video:secure_url" content={video.secureUrl.toString()} />
-        {/if}
-        {#if video.type}
-          <meta property="og:video:type" content={video.type.toString()} />
-        {/if}
-      {/each}
+    {#each openGraph.videos as video (video)}
+      <meta property="og:video" content={video.url} />
+      {#if video.width}
+        <meta property="og:video:width" content={video.width.toString()} />
+      {/if}
+      {#if video.height}
+        <meta property="og:video:height" content={video.height.toString()} />
+      {/if}
+      {#if video.secureUrl}
+        <meta property="og:video:secure_url" content={video.secureUrl.toString()} />
+      {/if}
+      {#if video.type}
+        <meta property="og:video:type" content={video.type.toString()} />
+      {/if}
+    {/each}
 
-      {#each openGraph.audio as audio (audio)}
-        <meta property="og:audio" content={audio.url} />
-        {#if audio.secureUrl}
-          <meta property="og:audio:secure_url" content={audio.secureUrl.toString()} />
-        {/if}
-        {#if audio.type}
-          <meta property="og:audio:type" content={audio.type.toString()} />
-        {/if}
-      {/each}
+    {#each openGraph.audio as audio (audio)}
+      <meta property="og:audio" content={audio.url} />
+      {#if audio.secureUrl}
+        <meta property="og:audio:secure_url" content={audio.secureUrl.toString()} />
+      {/if}
+      {#if audio.type}
+        <meta property="og:audio:type" content={audio.type.toString()} />
+      {/if}
+    {/each}
 
     {#if openGraph.locale}
       <meta property="og:locale" content={openGraph.locale} />
@@ -310,11 +310,11 @@
     {/if}
   {/if}
 
-    {#each additionalMetaTags as tag (tag)}
-      <meta {...tag.httpEquiv ? { ...tag, 'http-equiv': tag.httpEquiv } : tag} />
-    {/each}
+  {#each additionalMetaTags as tag (tag)}
+    <meta {...tag.httpEquiv ? { ...tag, 'http-equiv': tag.httpEquiv } : tag} />
+  {/each}
 
-    {#each additionalLinkTags as tag (tag)}
-      <link {...tag} />
-    {/each}
+  {#each additionalLinkTags as tag (tag)}
+    <link {...tag} />
+  {/each}
 </svelte:head>
